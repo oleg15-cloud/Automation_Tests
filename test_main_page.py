b3e8d0ce14292e9981816e5f17269e9867c867a7
oleg15-cloud <karaivanoleg093@gmail.com>
2020-04-09 11:20:10 +0300

    Create test: 'go_to_login_page'

--- test_main_page.py
+++ test_main_page.py
@@ -3,7 +3,7 @@ import pytest
 from .pages.main_page import MainPage
 
 
-def test_language(browser):
+def test_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
     page.open()
