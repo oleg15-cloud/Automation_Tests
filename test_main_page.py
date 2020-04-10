053354efb8849c4065d660558abf729caad48bea
oleg15-cloud <karaivanoleg093@gmail.com>
2020-04-10 16:01:17 +0300

    Change file

--- test_main_page.py
+++ test_main_page.py
@@ -7,6 +7,8 @@ def test_go_to_login_page(browser):
     page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)    # Создаем объект новой страницы, + помещаем туда url
+    login_page.should_be_login_page()                       # Проверяем странице на которую перешли
 
 
 def test_guest_should_see_login_link(browser):
