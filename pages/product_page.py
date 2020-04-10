5848f6a6ea0ccd9cd1445e4749639fa935433bbe
oleg15-cloud <karaivanoleg093@gmail.com>
2020-04-10 16:00:24 +0300

    Create new file

---
+++ pages/product_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        btn_add_to_basket = self.browser.find_element(*ProductPageLocators.BTN_ADD_TO_BASKET)
+        btn_add_to_basket.click()
+
+    def alert_math_finction(self):
+        self.solve_quiz_and_get_code()
+
+    def should_be_after_add_product_in_basket(self):
+        self.check_message_about_product_in_baskect()
+        self.check_product_price_in_message()
+
+    def check_message_about_product_in_baskect(self):
+        message = self.browser.find_element(*ProductPageLocators.MESSAGE).text
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_ON_PAGE).text
+        assert message == f"{product_name} был добавлен в вашу корзину.", "Not Found Message or Incorrect Product Name In Message"
+
+    def check_product_price_in_message(self):
+        product_price_in_message = self.browser.find_element(*ProductPageLocators.PRICE_IN_MESSAGE).text
+        product_price = self.browser.find_element(*ProductPageLocators.PRICE_ON_PAGE).text
+        assert product_price_in_message == product_price, "Incorrect Product Price"
