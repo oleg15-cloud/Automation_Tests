b7c443f1534656f6335118fa66722c9b3d20ccde
oleg15-cloud <karaivanoleg093@gmail.com>
2020-04-10 16:36:34 +0300

    Add paramentrize to test

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,13 @@
 from .pages.product_page import ProductPage
+import pytest
 import time
-
 # pytest -v --tb=line --language=en test_product_page.py
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('path', ["0", "1", "2", "3", "4", "5", "6",
+                                  pytest.param("7", marks=pytest.mark.xfail),
+                                  "8", "9"])
+def test_guest_can_add_product_to_basket(browser, path):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{path}"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
