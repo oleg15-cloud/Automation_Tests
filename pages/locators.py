9cd6af253db891ee49b94e3d3f0da88f576aec45
oleg15-cloud <karaivanoleg093@gmail.com>
2020-04-10 15:58:32 +0300

    Add new class: 'ProductPageLocators'

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,12 @@ class MainPageLocators():
 class LoginPageLocators():
     LOG_IN_FORM = (By.XPATH, "//form[@id='login_form']")
     REGISTRATION_FORM = (By.XPATH, "//form[@id='register_form']")
+
+
+class ProductPageLocators():
+    PRICE_ON_PAGE = (By.XPATH, "//div[@class='col-sm-6 product_main']/p[@class='price_color']")
+    PRICE_IN_MESSAGE = (By.XPATH, "//*[@id='messages']/div[3]/div/p[1]/strong")
+    MESSAGE = (By.XPATH, "//*[@id='messages']/div[1]/div")
+    PRODUCT_NAME_ON_PAGE = (By.XPATH, "//h1")
+    PRODUCT_NAME_IN_MESSAGE = (By.XPATH, "//*[@id='messages']//div[1]/div/strong")
+    BTN_ADD_TO_BASKET = (By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
