18f762d2799078eb5fe5b8fdc397d916c0528673
oleg15-cloud <karaivanoleg093@gmail.com>
2020-04-10 15:59:36 +0300

    Change file

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,11 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
     def should_be_login_url(self):
         login_url = self.browser.current_url
         assert login_url == "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/", "Login Url is incorrect"
