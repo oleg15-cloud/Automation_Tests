425696fa16d571a1a9bf6d6db90fa031db16b582
oleg15-cloud <karaivanoleg093@gmail.com>
2020-04-14 18:15:02 +0300

    Add new tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,13 @@
-from .pages.product_page import ProductPage
 import pytest
-import time
+from .pages.base_page import BasePage
+from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
+
 # pytest -v --tb=line --language=en test_product_page.py
 
+
+@pytest.mark.need_review
 @pytest.mark.parametrize('path', ["0", "1", "2", "3", "4", "5", "6",
                                   pytest.param("7", marks=pytest.mark.xfail),
                                   "8", "9"])
@@ -11,5 +16,59 @@ def test_guest_can_add_product_to_basket(browser, path):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
-    page.alert_math_finction()
+    page.alert_math_function()
     page.should_be_after_add_product_in_basket()
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/uk/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.alert_math_function()
+    page.should_not_be_message_after_add_product_in_basket()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/uk/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_message_after_add_product_in_basket()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/uk/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.alert_math_function()
+    page.succes_message_should_be_disappeared()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_link()
+
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    basket_page = BasketPage(browser, link)
+    basket_page.should_not_be_products_in_basket()
+    basket_page.should_be_text_about_empty_basket()
