e5bea60c1e3a941754d5dfef76140e9a7681e6d1
oleg15-cloud <karaivanoleg093@gmail.com>
2020-04-14 18:12:48 +0300

    Add new functions

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,27 @@
+import time
 from .base_page import BasePage
-from .locators import LoginPageLocators
+from .locators import LoginPageLocators, BasePageLocators
 
 
 class LoginPage(BasePage):
+    def create_and_check_register_new_user(self):
+        self.register_new_user()
+        self.should_be_authorized_user()
+
+
+    def register_new_user(self):
+        self.go_to_login_page()
+        self.browser.find_element(*LoginPageLocators.INPUT_REGISTER_EMAIL).send_keys(*LoginPageLocators.EMAIL)
+        self.browser.find_element(*LoginPageLocators.INPUT_REGISTER_PASSWORD).send_keys(*LoginPageLocators.PASSWORD)
+        self.browser.find_element(*LoginPageLocators.INPUT_CONFIRM_PASSWORD).send_keys(*LoginPageLocators.PASSWORD)
+        self.browser.find_element(*LoginPageLocators.BTN_REGISTER).click()
+
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                 " probably unauthorised user"
+
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
