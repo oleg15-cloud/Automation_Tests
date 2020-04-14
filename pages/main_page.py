01445324386c20e99244d7164f5a5d156f0c6ff3
oleg15-cloud <karaivanoleg093@gmail.com>
2020-04-14 18:13:45 +0300

    Change file

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,9 +3,6 @@ from .locators import MainPageLocators
 from .login_page import LoginPage
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)        # login_link - имя объекта на странице
-        login_link.click()
-
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
+        # заглушка! методы перемещенны в BasePage
